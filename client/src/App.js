518489eb81b3873133fb1f17e6f41f5381ca5ee4
FranklinMiranda <fdm61344@gmail.com>
2023-03-12 14:28:24 -0400

    CM

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import { useAuth0 } from '@auth0/auth0-react';
+
 import Home from './components/home';
 import LoginPage from './components/loginPage';
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
-import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
     <Auth0Provider
       domain="dev-yfnh5rsri6uihrxp.us.auth0.com"
       clientId="fzqgnVurba26VOn2X3mtENcDECis00sE"
@@ -15,5 +13,4 @@ root.render(
     >
       <App />
     </Auth0Provider>
-  </BrowserRouter>
 );
