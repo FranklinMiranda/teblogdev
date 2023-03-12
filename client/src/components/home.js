58c102de1e04d1ccb86fb17db1c9d95b2113c548
FranklinMiranda <fdm61344@gmail.com>
2023-03-12 13:54:46 -0400

    CM

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -3,7 +3,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from './login';
 import LogoutButton from './logout';
 import Profile from './profile';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 
 const Home = () => {
   const { isAuthenticated, isLoading, user } = useAuth0();
@@ -33,8 +33,19 @@ const Home = () => {
     return (
       <div>
         <h1>Welcome to Time Expeditions Blog!</h1>
-        <Profile />
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/profile">Profile</Link></li>
+            </ul>
+        </nav>
         <LogoutButton />
+
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+        </Routes>
+
       </div>
     );
   }
