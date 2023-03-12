cfcd6b1c302f732d0c339e2efe4ee99592a71d2a
FranklinMiranda <fdm61344@gmail.com>
2023-03-12 15:51:28 -0400

    Axios Request Moved to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import { useAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
 
 import Home from './components/home';
 import LoginPage from './components/loginPage';
 
 function App() {
-  const { isAuthenticated, isLoading } = useAuth0();
+  const { isAuthenticated, isLoading, user } = useAuth0();
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -17,6 +18,15 @@ function App() {
       </div>
     );
   } else if (isAuthenticated) {
+    axios
+      .post('/api/userprofiletodb', user)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
     return (
       <div className="App">
         <Home />
--- client/src/components/auth0/profile.js
+++ client/src/components/auth0/profile.js
@@ -1,20 +1,8 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
-import axios from 'axios';
 
 const Profile = () => {
   const { user, isAuthenticated } = useAuth0();
-
-  if (isAuthenticated) {
-    axios
-      .post('/api/userprofiletodb', user)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
 
   return (
     isAuthenticated && (
