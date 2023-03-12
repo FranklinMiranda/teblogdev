b02942f8a661ea6993fca32332ef814c5026fe18
FranklinMiranda <fdm61344@gmail.com>
2023-03-12 10:38:20 -0400

    CM

--- server/main/app.js
+++ server/main/app.js
@@ -8,12 +8,6 @@ var indexRouter = require('./routes');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
