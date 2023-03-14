a5d352e9b051305e5d8fa468545d53f4ec602f4c
FranklinMiranda <fdm61344@gmail.com>
2023-03-14 12:18:20 -0400

    DB Schema Update

--- server/main/schema.sql
+++ server/main/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users ( uid SERIAL PRIMARY KEY, username VARCHAR(255) UNIQUE, email VARCHAR(255), name VARCHAR(255), date_created DATE);
 
-CREATE TABLE posts ( pid SERIAL PRIMARY KEY, title VARCHAR(255), body VARCHAR, user_id INT REFERENCES users(uid), author VARCHAR REFERENCES users(username), date_created TIMESTAMP);
+CREATE TABLE posts ( pid SERIAL PRIMARY KEY, title VARCHAR(255), body VARCHAR, user_id INT REFERENCES users(uid), author VARCHAR REFERENCES users(username), date_created TIMESTAMP, liked_by VARCHAR(255)[] DEFAULT ARRAY[]::VARCHAR(255)[]);
 
 CREATE TABLE comments ( cid SERIAL PRIMARY KEY, comment VARCHAR(255), author VARCHAR REFERENCES users(username), user_id INT REFERENCES users(uid), post_id INT REFERENCES posts(pid), date_created TIMESTAMP);
