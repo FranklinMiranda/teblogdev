5caff1dfb40e84e26d6dfec1a8412c08ead57455
FranklinMiranda <fdm61344@gmail.com>
2023-03-14 19:58:53 -0400

    Update to ACTION TYPES

--- client/src/components/store/actions/action_types.js
+++ client/src/components/store/actions/action_types.js
@@ -3,6 +3,8 @@ export const ADD_DB_PROFILE = 'ADD_DB_PROFILE';
 
 export const REMOVE_DB_PROFILE = 'REMOVE_DB_PROFILE';
 
+// post_reducer Action Types
+
 export const FETCH_DB_POSTS = "FETCH_DB_POSTS"
 
 export const REMOVE_DB_POSTS = "REMOVE_DB_POSTS"
