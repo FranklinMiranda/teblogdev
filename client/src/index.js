7bf4fb8bde6eb83289725595f782f516bbd7e9d5
FranklinMiranda <fdm61344@gmail.com>
2023-08-17 13:52:51 -0400

    Production and AUTH0 Updates

--- client/src/index.js
+++ client/src/index.js
@@ -14,8 +14,8 @@ root.render(
   <BrowserRouter>
     <Auth0Provider
       domain="dev-yfnh5rsri6uihrxp.us.auth0.com"
-      clientId="fzqgnVurba26VOn2X3mtENcDECis00sE"
-      authorizationParams={{ redirect_uri: 'http://localhost:3000' }}
+      clientId="mGc24ls7sPEKurfFLvGbUOqTxY4ON1e0"
+      authorizationParams={{ redirect_uri: 'https://timeexpeditions.com' }}
     >
       <Provider store={store}>
         <Landing />
