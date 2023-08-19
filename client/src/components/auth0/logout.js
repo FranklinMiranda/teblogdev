7a96f4d45c47e706db4c74c21f0a7a046503e3cc
FranklinMiranda <fdm61344@gmail.com>
2023-08-19 18:43:36 -0400

    Updates to Callback URL

--- client/src/components/auth0/logout.js
+++ client/src/components/auth0/logout.js
@@ -3,7 +3,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
-  return <button className="Button" onClick={() => logout({ logoutParams: { returnTo: 'http://localhost' } })}>Log Out</button>;
+  return <button className="Button" onClick={() => logout({ logoutParams: { returnTo: 'http://localhost:3000' } })}>Log Out</button>;
 };
 
 export default LogoutButton;
